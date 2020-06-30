5239a5d14f1b30a8fe8c2cfc72fa60c8c2bdddb6
Hyoek <hyc7575@naver.com>
2020-06-30 16:17:34 +0900

    props size

--- src/Icon.js
+++ src/Icon.js
@@ -3,13 +3,6 @@ import PropTypes from "prop-types";
 
 import * as eva from "eva-icons";
 
-const SIZE = {
-  SMALL: "12px",
-  MEDIUM: "18px",
-  LARGE: "24px",
-  XLARGE: "30px",
-};
-
 class Icon extends Component {
   componentDidMount() {
     this.setupEvaIcons();
@@ -22,44 +15,25 @@ class Icon extends Component {
   setupEvaIcons() {
     const { fill, animation, size } = this.props;
 
-    const dims = this.updateDims(size);
-
     const config = {
       fill,
-      width: dims,
-      height: dims,
+      width: size,
+      height: size,
       animation,
     };
 
     eva.replace(config);
   }
 
-  updateDims(size) {
-    switch (size) {
-      case "small":
-        return SIZE.SMALL;
-      case "medium":
-        return SIZE.MEDIUM;
-      case "large":
-        return SIZE.LARGE;
-      case "xlarge":
-        return SIZE.XLARGE;
-      default:
-        return SIZE.MEDIUM;
-    }
-  }
-
   render() {
     const { name, fill, size, animation } = this.props;
-
-    const dims = this.updateDims(size);
 
     const icon = (
       <i
         data-eva={name}
         data-eva-fill={fill}
-        data-eva-height={dims}
-        data-eva-width={dims}
+        data-eva-height={size}
+        data-eva-width={size}
       />
     );
 
