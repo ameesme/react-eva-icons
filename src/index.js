e8fd6315d4a5bf0972d70d3070d5d8b2b9d2665b
weddingbook <info@weddingbook.com>
2020-07-13 13:54:51 +0900

    remove icon.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,66 @@
  * @license MIT
  */
 
-import Icon from './Icon';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import * as eva from "eva-icons";
+
+class Icon extends Component {
+	componentDidMount() {
+		this.setupEvaIcons();
+	}
+
+	componentDidUpdate() {
+		this.setupEvaIcons();
+	}
+
+  	setupEvaIcons() {
+		const { fill, animation, size } = this.props;
+
+		const config = {
+			fill,
+			width: size,
+			height: size,
+			animation,
+		};
+
+		eva.replace(config);
+	}
+
+	render() {
+		const { name, fill, size, animation } = this.props;
+		const { type, hover = false, infinite = false } = animation;
+		const icon = (
+			<i
+				data-eva={name}
+				data-eva-fill={fill}
+				data-eva-height={size}
+				data-eva-width={size}
+				{...(type !== undefined ? {
+					'data-eva-animation': type,
+					'data-eva-hover': hover,
+					'data-eva-infinite': infinite
+				} : {})
+				}
+			/>
+		);
+		return icon;
+	}
+}
+
+Icon.propTypes = {
+	animation: PropTypes.object,
+	fill: PropTypes.string,
+	name: PropTypes.string,
+	size: PropTypes.string,
+};
+
+Icon.defaultProps = {
+	animation: {},
+	fill: "#fff",
+	name: "",
+	size: "20",
+};
+
 export default Icon;
